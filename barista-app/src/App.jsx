eb2d770235df7c9d7f01d08e29fe1ad02e1b68fc
Xujuan Chen <xchen022@citymail.cuny.edu>
2023-03-08 19:49:27 -0500

    step 1

--- barista-app/src/App.jsx
+++ barista-app/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
+import BaristaForm from './components/baristaForm';
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
-
+      <div className="title-container">
+        <h1 className="title">On My Grind</h1>
+        <p>So you think you can barista? Let's put that to the test...</p>
+      </div>
+      <BaristaForm />
     </div>
   )
 }
